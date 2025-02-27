f161a9f930538d1d7da26743cf911ff80a518ecf
Yusaku Nagatani <64152685+Yusaku01@users.noreply.github.com>
2025-02-27 22:47:19 +0900

    fix: プロフィール画面のUI大幅に修正

---
+++ src/data/profileData.ts
@@ -0,0 +1,114 @@
+export const ProfileContent = {
+  name: '長谷 優作（Saku）',
+  role: 'Webコーダー/フロントエンドエンジニア',
+  avatar: '/images/img_profile.png',
+  coverImage:
+    'https://images.unsplash.com/photo-1550645612-83f5d594b671?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
+  bio: '写真を撮ることに興味を持ったことがきっかけで、いつしかWebデザインの世界へと足を踏み入れました。Webサイトをデザインする過程で、コーディングの楽しさに気づき、次第にフロントエンド開発にのめり込んでいきました。HTML/CSSの静的なコードを書いているうちに、業務をよりスムーズに進めるためには開発フロー全体を理解する必要性を痛感。現在は個人学習で開発全体のプロセスやネットワークの基礎知識を学び直しています。また、好奇心から生成AIツールにも触れており、新しい技術に常にアンテナを張っています。',
+  professionalSkills: [
+    {
+      name: 'HTML',
+      years: 3,
+      details: 'HTML5, セマンティックマークアップ, ウェブアクセシビリティ, SEO最適化',
+    },
+    {
+      name: 'CSS',
+      years: 3,
+      details:
+        'CSS3, Flexbox, Grid, UIのインタラクション, 簡単なアニメーション, レスポンシブデザイン',
+    },
+    {
+      name: 'JavaScript',
+      years: 3,
+      details: 'ES6+, DOM操作, 非同期処理, API連携',
+    },
+    {
+      name: 'jQuery',
+      years: 3,
+      details: '簡単なUIのアニメーション, イベント処理',
+    },
+    {
+      name: 'Sass/SCSS',
+      years: 2,
+      details: 'mixin, ネスト, 変数',
+    },
+    {
+      name: 'TailwindCSS',
+      years: 2,
+      details:
+        'ユーティリティファースト, カスタム設定, ダークモード対応, Sass/SCSSとの組み合わせ記述',
+    },
+    {
+      name: 'Git（Sourcetree/GitHub等コラボレーションツールも含む）',
+      years: 3,
+      details: 'リビション管理, ファイル差分管理, Git連携/運用',
+    },
+    {
+      name: 'Backlog',
+      years: 2,
+      details: 'クライアントさんとのプロジェクト管理, Gitリポジトリ管理/運用',
+    },
+    {
+      name: 'Figma',
+      years: 2,
+      details: 'UIの設計, Figjamを使用したワークショップ',
+    },
+    {
+      name: 'AdobeCreativeCloud',
+      years: 3,
+      details: 'Photoshop, Illustrator, XDなどを使用してデザイン作成を行なっていた時もありました。',
+    },
+  ],
+  personalSkills: [
+    {
+      name: 'TypeScript',
+      years: 2,
+      details: '型定義, インターフェース, ジェネリクス',
+    },
+    {
+      name: 'React',
+      years: 1,
+      details: 'Hooks, コンポーネント設計, 状態管理',
+    },
+    {
+      name: 'Astro',
+      years: 1,
+      details: 'コンポーネント設計, アイランドアーキテクチャ, ページルーティング, 一部React導入',
+    },
+    {
+      name: 'Next.js',
+      years: 1,
+      details: 'SSR, SSG, API Routes',
+    },
+  ],
+  socialLinks: [
+    {
+      name: 'GitHub',
+      url: 'https://github.com/Yusaku01',
+      iconLight: '/images/icon_github-light.svg',
+      iconDark: '/images/icon_github-dark.svg',
+      class: 'w-6 h-6',
+    },
+    {
+      name: 'X',
+      url: 'https://x.com/Saku37215349',
+      iconLight: '/images/icon_x-light.svg',
+      iconDark: '/images/icon_x-dark.svg',
+      class: 'w-5 h-5',
+    },
+    {
+      name: 'LinkedIn',
+      url: 'https://www.linkedin.com/in/yusaku-nagatani-9325ab238/',
+      iconLight: '/images/icon_linkedin.svg',
+      iconDark: '/images/icon_linkedin.svg',
+      class: 'w-5 h-5',
+    },
+    {
+      name: 'Qiita',
+      url: 'https://qiita.com/ngtnysk',
+      iconLight: '/images/icon_qiita.png',
+      iconDark: '/images/icon_qiita.png',
+      class: 'w-5 h-5',
+    },
+  ],
+};
