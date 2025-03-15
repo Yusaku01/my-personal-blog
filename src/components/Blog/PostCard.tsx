a9d29eb554a3aca9565f02b18635338413f8261b
Yusaku Nagatani <64152685+Yusaku01@users.noreply.github.com>
2025-03-15 19:34:57 +0900

    改善: ブログ関連コンポーネントのスタイル調整

--- src/components/Blog/PostCard.tsx
+++ src/components/Blog/PostCard.tsx
@@ -7,18 +7,20 @@ type PostCardProps = Post & {
 // PostCardコンポーネント固有のスタイル
 const postCardStyles = {
   article:
-    'bg-gray-50 dark:bg-gray-800 rounded-md overflow-hidden transition-all duration-200 h-full hover',
+    'bg-white dark:bg-gray-900 rounded-md overflow-hidden transition-all duration-200 h-full hover',
   link: 'block',
   imageContainer: 'relative aspect-video overflow-hidden', // overflow-hiddenを維持
   image: 'w-full h-full object-cover transition-all duration-300 ease-in-out', // 拡大を削除
-  overlay: 'absolute inset-0 bg-black bg-opacity-0 flex items-center justify-center transition-all duration-300 ease-in-out group-hover:bg-opacity-60', // オーバーレイを追加
-  readText: 'text-white font-medium opacity-0 transform translate-y-4 transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:translate-y-0', // 「記事を読む」テキスト
+  overlay:
+    'absolute inset-0 bg-black bg-opacity-0 flex items-center justify-center transition-all duration-300 ease-in-out group-hover:bg-opacity-60', // オーバーレイを追加
+  readText:
+    'text-white font-bold opacity-0 transform translate-y-4 transition-all duration-300 ease-in-out group-hover:opacity-100 group-hover:translate-y-0 tracking-wider', // 「記事を読む」テキスト
   contentContainer: 'p-4',
   title: 'text-md font-semibold text-gray-900 dark:text-white line-clamp-4 leading-tight',
   date: 'text-sm text-gray-600 dark:text-gray-400',
   excerpt: 'text-sm text-gray-600 dark:text-gray-400 line-clamp-2',
   tagContainer: 'flex flex-wrap gap-2',
-  tag: 'inline-flex items-center px-2.5 py-0.5 rounded-full text-2.5 font-medium bg-gray-100 border border-gray-300 dark:bg-gray-700 dark:border-gray-600 text-gray-700 dark:text-gray-200',
+  tag: 'inline-flex items-center px-2.5 py-0.5 rounded-full text-2.5 font-medium border border-gray-900 dark:bg-gray-400 dark:border-gray-600 text-gray-700 dark:text-gray-200',
 };
 
 export function PostCard({
