64806cb85f74b7b958bcf64f298256126f972424
Yusaku Nagatani <64152685+Yusaku01@users.noreply.github.com>
2025-03-15 19:34:27 +0900

    改善: プロフィールページのレイアウトとデータ更新

--- src/lib/utils/profileData.ts
+++ src/lib/utils/profileData.ts
@@ -1,68 +1,49 @@
 export const ProfileContent = {
   name: '長谷 優作（Saku）',
-  role: 'Webコーダー/フロントエンドエンジニア',
+  role: 'フロントエンドエンジニア / デザイナー',
   avatar: '/images/img_profile.png',
   coverImage:
     'https://images.unsplash.com/photo-1550645612-83f5d594b671?ixlib=rb-1.2.1&auto=format&fit=crop&w=1000&q=80',
-  bio: '写真を撮ることに興味を持ったことがきっかけで、いつしかWebデザインの世界へと足を踏み入れました。Webサイトをデザインする過程で、コーディングの楽しさに気づき、次第にフロントエンド開発にのめり込んでいきました。HTML/CSSの静的なコードを書いているうちに、業務をよりスムーズに進めるためには開発フロー全体を理解する必要性を痛感。現在は個人学習で開発全体のプロセスやネットワークの基礎知識を学び直しています。また、好奇心から生成AIツールにも触れており、新しい技術に常にアンテナを張っています。',
+  bio: '写真を撮ることに興味を持ったことがきっかけで、いつしかWebデザインの世界へと足を踏み入れました。Webサイトをデザインする過程で、コーディングの楽しさに気づき、次第にフロントエンド開発に興味を持っていきました。HTML/CSSの静的なコードを書いているうちに、業務をよりスムーズに進めるためには開発フロー全体を理解する必要性を痛感。最近は好奇心から生成AIツールにも触れており、新しい技術に常にアンテナを張っています。最新技術に触れるたび、CS基礎の重要性を感じ、個人学習で開発全体のプロセスやネットワークの基礎知識を学び直しています。',
   professionalSkills: [
     {
       name: 'HTML',
-      years: 3,
-      details: 'HTML5, セマンティックマークアップ, ウェブアクセシビリティ, SEO最適化',
+      details:
+        'HTML5, セマンティックマークアップ, ウェブアクセシビリティ（学習中）, SEO最適化（学習中）',
     },
     {
       name: 'CSS',
-      years: 3,
       details:
-        'CSS3, Flexbox, Grid, UIのインタラクション, 簡単なアニメーション, レスポンシブデザイン',
-    },
-    {
-      name: 'JavaScript',
-      years: 3,
-      details: 'ES6+, DOM操作, 非同期処理, API連携',
-    },
-    {
-      name: 'jQuery',
-      years: 3,
-      details: '簡単なUIのアニメーション, イベント処理',
-    },
-    {
-      name: 'Sass/SCSS',
-      years: 2,
-      details: 'mixin, ネスト, 変数',
+        'CSS3, ,Sass/SCSS, Flexbox, Grid, UIインタラクション, 簡単なアニメーション, レスポンシブデザイン',
     },
     {
       name: 'TailwindCSS',
-      years: 2,
-      details:
-        'ユーティリティファースト, カスタム設定, ダークモード対応, Sass/SCSSとの組み合わせ記述',
+      details: 'レスポンシブデザイン, カスタム設定, ダークモード対応, Sass/SCSSとの組み合わせ記述',
+    },
+    {
+      name: 'JavaScript',
+      details: 'ES6+, DOM操作, 非同期処理, API連携, jQuery',
     },
     {
       name: 'Git（Sourcetree/GitHub等コラボレーションツールも含む）',
-      years: 3,
       details: 'リビション管理, ファイル差分管理, Git連携/運用',
     },
     {
       name: 'Backlog',
-      years: 2,
       details: 'クライアントさんとのプロジェクト管理, Gitリポジトリ管理/運用',
     },
     {
       name: 'Figma',
-      years: 2,
       details: 'UIの設計, Figjamを使用したワークショップ',
     },
     {
       name: 'AdobeCreativeCloud',
-      years: 3,
       details: 'Photoshop, Illustrator, XDなどを使用してデザイン作成を行なっていた時もありました。',
     },
   ],
   personalSkills: [
     {
       name: 'TypeScript',
-      years: 2,
       details: '型定義, インターフェース, ジェネリクス',
     },
     {
@@ -73,7 +54,8 @@ export const ProfileContent = {
     {
       name: 'Astro',
       years: 1,
-      details: 'コンポーネント設計, アイランドアーキテクチャ, ページルーティング, 一部React導入',
+      details:
+        'コンポーネント設計, アイランドアーキテクチャ, ページルーティング, Reactと組み合わせて使用',
     },
     {
       name: 'Next.js',
