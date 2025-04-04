90d36f0f7f4256865c821168a2c7e6def2026135
Yusaku Nagatani <64152685+Yusaku01@users.noreply.github.com>
2025-04-05 00:43:30 +0900

    fix: Google Analytics読み込み

--- src/env.d.ts
+++ src/env.d.ts
@@ -2,3 +2,23 @@
 /* Astro types import */
 import type {} from '../.astro/types';
 /// <reference types="astro/client" />
+
+// WindowオブジェクトにdataLayerプロパティを追加
+interface Window {
+  dataLayer: any[]; // または Array<any> や、より具体的な型
+}
+
+// gtag関数をグローバルに追加
+declare global {
+  function gtag(...args: any[]): void; // any[] はより具体的な型にすることも可能
+}
+
+// Google Analytics IDを環境変数から取得
+interface ImportMetaEnv {
+  readonly PUBLIC_GOOGLE_ANALYTICS_ID: string;
+}
+
+// ImportMetaEnvをグローバルに追加
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
