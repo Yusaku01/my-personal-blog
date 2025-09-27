5d90a9c52da739f9df90d5bf4fe6f8efe4099614
Yusaku Nagatani <64152685+Yusaku01@users.noreply.github.com>
2025-09-27 14:07:53 +0900

    fix: 一部属性をReactとの互換性部分の修正

--- src/components/Blog/PostCard.tsx
+++ src/components/Blog/PostCard.tsx
@@ -58,7 +58,7 @@ export function PostCard({
               height={315}
               className={postCardStyles.image}
               loading={shouldLazyLoad ? 'lazy' : undefined}
-              fetchPriority={fetchPriority}
+              fetchpriority={fetchPriority}
               decoding="async"
               sizes="(max-width: 640px) 100vw, (max-width: 1024px) 50vw, 25vw"
             />
