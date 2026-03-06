26282aebebfaf85792744467583e060200dc024f
Yusaku Nagatani <64152685+Yusaku01@users.noreply.github.com>
2026-03-06 12:19:05 +0900

    style: implement fluid card layout for blog and feeds sections

--- uno.config.ts
+++ uno.config.ts
@@ -131,7 +131,6 @@ export default defineConfig({
       'w-full px-4 py-2 border border-gray-200 rounded-md focus:outline-none focus:ring-2 focus:ring-gray-500 bg-white dark:bg-white dark:border-none',
     'blog-search-button':
       'absolute top-0 right-0 px-4 py-2 text-sm bg-gray-800 text-white lg:hover:opacity-70 transition-all duration-300 rounded-r-md h-full font-bold leading-loose dark:border-none',
-    'blog-post-grid': 'grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-4',
     'blog-load-more-button':
       'mx-auto mt-8 px-6 py-2 bg-gray-800 text-white rounded-md lg:hover:opacity-70 transition-all duration-300 dark:bg-gray-200 dark:text-gray-800 font-bold',
 
